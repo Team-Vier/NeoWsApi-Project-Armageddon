e8447a0c16702f769fd344f89584f4862f6de7e4
Viktoria Heinen <VHeinen@conet.de>
2024-01-15 12:44:31 +0100

    add api connection

--- src/main/java/org/team4/Main.java
+++ src/main/java/org/team4/Main.java
@@ -1,18 +1,18 @@
 package org.team4;
 
 
+import org.team4.repositories.NasaDataRepository;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+         NasaDataRepository nasaDataRepository = new NasaDataRepository("DEMO_KEY");
+
+        nasaDataRepository.getAPIResponse();
     }
 }
--- src/main/java/org/team4/repositories/NasaDataRepository.java
+++ src/main/java/org/team4/repositories/NasaDataRepository.java
@@ -1,4 +1,33 @@
 package org.team4.repositories;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class NasaDataRepository {
+
+    public String apiKey;
+
+    public void getAPIResponse () throws URISyntaxException, IOException, InterruptedException {
+        String apiUri = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=" + apiKey;
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI(apiUri))
+                .header("Authorization", apiKey).build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> getResponse = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(getResponse.body());
+    }
+
+    public NasaDataRepository(String apiKey) {
+        this.apiKey = apiKey;
+    }
 }
+
+
